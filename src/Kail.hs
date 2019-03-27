ddd121b831863810f046fab41dfb5be0314077ae
Max <mushishi78@gmail.com>
2019-03-27 22:24:50 +0000

    Initial parsing with Text.ParserCombinators.ReadP
    Just parsing the first line for the schema declaration

--- src/Kail.hs
+++ src/Kail.hs
@@ -2,6 +2,20 @@ module Kail
     ( parse
     ) where
 
+import Text.ParserCombinators.ReadP
+
+data KailFile =
+    KailFile Schema deriving Show
+
+data Schema = Schema String deriving Show
+
 parse :: String -> Bool -> IO ()
-parse contents bail = putStrLn "hello"
+parse contents bail = putStrLn $ show $ readP_to_S kailFile contents
+
+kailFile :: ReadP KailFile
+kailFile = do
+    string "kail "
+    schema <- Schema <$> munch (\c -> c /= '\n')
+    return $ KailFile schema
+
 
