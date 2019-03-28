03ba46b2ccd171515aa3991a9c08337a27b37988
Max <mushishi78@gmail.com>
2019-03-28 00:46:33 +0000

    Start parsing into lines
    Also changed import and comment syntax
    Also added a more simple example
    Also started using linter and formatter

--- app/Main.hs
+++ app/Main.hs
@@ -14,15 +14,13 @@
 
 {-# LANGUAGE
     OverloadedStrings,
-    ScopedTypeVariables,
-    QuasiQuotes,
-    LambdaCase
+    ScopedTypeVariables
 #-}
 
 module Main where
 
 import qualified Kail
-import Options.Applicative
+import           Options.Applicative
 
 data Command
     = Validate ValidateOptions
@@ -33,7 +31,7 @@ data ValidateOptions = ValidateOptions {
     bail :: Bool
 }
 
-data NakedOptions = NakedOptions {
+newtype NakedOptions = NakedOptions {
     version :: Bool
 }
 
@@ -42,30 +40,33 @@ main = execParser (info parser mempty) >>= run
 
 parser :: Parser Command
 parser =
-    subparser (command "validate" (info validateParser (progDesc "Validate a kail file")))
-    <|> nakedParser
+    subparser
+            (command "validate"
+                     (info validateParser (progDesc "Validate a kail file"))
+            )
+        <|> nakedParser
   where
-    validateParser = Validate <$> (ValidateOptions <$> fileOption <*> bailOption) <**> helper
+    validateParser =
+        Validate <$> (ValidateOptions <$> fileOption <*> bailOption) <**> helper
     nakedParser = Naked <$> (NakedOptions <$> versionOption) <**> helper
 
     fileOption :: Parser (Maybe String)
-    fileOption = optional $ strOption $
-        long "file" <> short 'f' <> metavar "FILE" <> help "FILE to be validated"
-    bailOption = switch $
-        long "bail" <> short 'b' <> help "Stop when the first error is found"
-    versionOption = switch $
-        long "version" <> short 'v' <> help "Display the version of kail"
+    fileOption =
+        optional
+            $  strOption
+            $  long "file"
+            <> short 'f'
+            <> metavar "FILE"
+            <> help "FILE to be validated"
+    bailOption = switch $ long "bail" <> short 'b' <> help
+        "Stop when the first error is found"
+    versionOption = switch $ long "version" <> short 'v' <> help
+        "Display the version of kail"
 
 run :: Command -> IO ()
 run (Validate opts) = do
     contents <- case file opts of
-        Just f -> readFile f
+        Just f  -> readFile f
         Nothing -> getContents
     Kail.parse contents (bail opts)
-run (Naked opts) =
-    if version opts then
-        putStrLn "1.0.0"
-    else
-        putStrLn "Naked!"
-            
-            
+run (Naked opts) = if version opts then putStrLn "1.0.0" else putStrLn "Naked!"
--- src/Kail.hs
+++ src/Kail.hs
@@ -1,21 +1,39 @@
 module Kail
     ( parse
-    ) where
+    )
+where
 
-import Text.ParserCombinators.ReadP
+import           Text.ParserCombinators.ReadP
+import           Control.Applicative            ( (<|>) )
 
-data KailFile =
-    KailFile Schema deriving Show
-
-data Schema = Schema String deriving Show
+data Line = Line (Maybe Char) Int String | Empty deriving Show
 
 parse :: String -> Bool -> IO ()
-parse contents bail = putStrLn $ show $ readP_to_S kailFile contents
+parse contents bail = print lines
+    where lines = takeLast $ readP_to_S (many (line <|> empty)) contents
 
-kailFile :: ReadP KailFile
-kailFile = do
-    string "kail "
-    schema <- Schema <$> munch (\c -> c /= '\n')
-    return $ KailFile schema
+takeLast :: [(a, String)] -> Maybe a
+takeLast s = if remainder == "" then Just result else Nothing
+    where (result, remainder) = last s
 
+line :: ReadP Line
+line = do
+    indent  <- munch (== ' ')
+    op      <- maybs $ satisfy isOperator
+    content <- munch1 (\ch -> ch /= '\r' && ch /= '\n')
+    newline
+    return $ Line op (length indent) content
 
+empty :: ReadP Line
+empty = newline >> return Empty
+
+maybs :: ReadP a -> ReadP (Maybe a)
+maybs p = (Just <$> p) <++ return Nothing
+
+isOperator :: Char -> Bool
+isOperator ch = ch == ':' || ch == '#' || ch == '>'
+
+newline :: ReadP ()
+newline = do
+    string "\r\n" <++ string "\r" <|> string "\n"
+    return ()
