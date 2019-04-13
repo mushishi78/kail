883d51d285fa453f0080c7c8e1fa66b6a54f29a3
Max <mushishi78@gmail.com>
2019-04-14 00:11:40 +0100

    Refactor main run to use ExceptT

--- app/Main.hs
+++ app/Main.hs
@@ -21,15 +21,27 @@ module Main where
 
 import qualified Kail
 import           Options.Applicative
-import Filesystem.Path.CurrentOS
+import qualified Filesystem.Path.CurrentOS as FP
 import System.Directory
+import Control.Monad.Trans.Except
+import Control.Monad.IO.Class
+import Control.Exception
+import Data.Bifunctor
 
 data Command
     = Validate ValidateOptions
     | Naked NakedOptions
 
+data MainError =
+    DataFileReadFail |
+    ParseFail |
+    ParseErrors Kail.AST |
+    SchemaFileReadFail
+
+type MainT a = ExceptT MainError IO a
+
 data ValidateOptions = ValidateOptions {
-    file :: Maybe String
+    dataFilePath :: Maybe String
 }
 
 newtype NakedOptions = NakedOptions {
@@ -48,11 +60,11 @@ parser =
         <|> nakedParser
   where
     validateParser =
-        Validate <$> (ValidateOptions <$> fileOption) <**> helper
+        Validate <$> (ValidateOptions <$> dataFileOption) <**> helper
     nakedParser = Naked <$> (NakedOptions <$> versionOption) <**> helper
 
-    fileOption :: Parser (Maybe String)
-    fileOption =
+    dataFileOption :: Parser (Maybe String)
+    dataFileOption =
         optional
             $  strOption
             $  long "file"
@@ -63,21 +75,51 @@ parser =
         "Display the version of kail"
 
 run :: Command -> IO ()
-run (Validate opts) = do
-    contents <- case file opts of
-        Just f  -> readFile f
-        Nothing -> getContents
-    cwd <- case file opts of
-        Just f  -> return $ parent (decodeString f)
-        Nothing -> decodeString <$> getCurrentDirectory
-    case Kail.parse contents of
-        Nothing -> putStrLn "Parse failure"
-        (Just ast) -> case Kail.errors ast of
-            (e:errs) -> print (e:errs)
-            [] -> case Kail.schema ast of
-                Nothing -> return ()
-                (Just path) -> do
-                    schemaContents <- readFile $ encodeString $ cwd <> (decodeString path)
-                    print ast
-                    print schemaContents
 run (Naked opts) = if version opts then putStrLn "1.0.0" else putStrLn "Naked!"
+run (Validate opts) = either renderError return =<< runExceptT run'
+    where
+        run' :: MainT ()
+        run' = do
+            -- Read the kail data file either from provided file or piped in
+            dataFile <- safeIO DataFileReadFail $ maybe getContents readFile $ dataFilePath opts
+
+            -- Get the directory that the schema path will be relative to
+            -- So when the path to the data file is provided, this will be that directory
+            -- Otherwise the current working directory is used
+            dir <- case dataFilePath opts of
+                (Just f) -> ExceptT $ return $ Right $ FP.parent $ FP.decodeString $ f
+                Nothing -> liftIO $ FP.decodeString <$> getCurrentDirectory
+
+            -- Parse the data file into an ast
+            ast <- ExceptT $ return $ maybe (Left ParseFail) Right $ Kail.parse dataFile
+
+            -- Do not continue if there are parse errors
+            _ <- ExceptT $ return $ if null (Kail.errors ast) then Right () else Left $ ParseErrors ast
+
+            -- Read the schema file
+            let schemaPath = FP.decodeString $ ignoreNothing $ Kail.schema ast
+            let schemaPathFromCWD = FP.encodeString $ dir <> schemaPath
+            schemaFile <- safeIO SchemaFileReadFail $ readFile schemaPathFromCWD
+
+            liftIO $ print ast
+            liftIO $ print schemaFile
+            return ()
+
+renderError :: MainError -> IO ()
+renderError err = case err of
+    DataFileReadFail -> putStrLn "Failed to read kail data file"
+    ParseFail -> putStrLn "Failed to parse kail data file"
+    (ParseErrors ast) -> do
+        putStrLn $ "Parse errors: " ++ show (Kail.errors ast)
+        putStrLn $ "Parse warnings: " ++ show (Kail.warnings ast)
+    SchemaFileReadFail -> putStrLn "Failed to read kail schema file"
+
+safeIO :: MainError -> IO a -> MainT a
+safeIO err io = ExceptT $ liftIO $ try io >>= return . (first mapError)
+    where
+        mapError :: IOException -> MainError
+        mapError _ = err
+
+ignoreNothing :: Maybe a -> a
+ignoreNothing (Just a) = a
+ignoreNothing Nothing = undefined
